90affefaf8cbb20c20071eadb9c6c0ae86aaa2e4
[tseferi] <[teala.seferi@cns.uist.edu.mk]>
2019-11-01 16:36:14 +0100

    Unused factory bot and updated bundle

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,9 +32,21 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+begin
+  require 'factory_bot_rails'
+rescue LoadError
+end
+
+class MyEngine < ::Rails::Engine
+  # if defined?(FactoryBotRails)
+  #   config.factory_bot.definition_file_paths +=
+  #     [File.expand_path('../factories', __FILE__)]
+  # end
+end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -60,4 +72,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # config.factory_bot.definition_file_paths = ["custom/factories"]
 end
