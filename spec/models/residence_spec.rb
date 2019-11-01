c60f93522db39df9deba32ab34c65627e89c0867
[tseferi] <[teala.seferi@cns.uist.edu.mk]>
2019-11-01 11:31:35 +0100

    added ruby-version and Rspec code in the spec models

---
+++ spec/models/residence_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+require 'rspec/rails'
+
+# RSpec.describe Residence, :type => :model do
+#   it 'is valid with valid attributes' do
+#     expect(Residence.new).to be_valid
+#   end
+  # it 'is not valid without a title' do
+  #   Residence.new(:residence, title: nil)
+  #   expect(Residence).to_not be_valid
+  # end
+  # it 'is not valid without a description' do
+  #   Residence.new(description: true)
+  #   expect(Residence).to_not be_valid
+  # end
+  # it 'is not valid without a start_date' do
+  #   Residence.new(start_date: nil)
+  #   expect(Residence).to_not be_valid
+  # end
+  # it 'is not valid without a end_date' do
+  #   Residence.new(end_date: nil)
+  #   expect(Residence).to_not be_valid
+  # end
+# end
+RSpec.describe Residence, :type => :model do
+  it 'validates presence'do
+    record = Residence.new
+    record.category_id
+    record.title = ' ' #invalid state
+    record.valid? #run validations
+    record.errors[:title]= :should, include("can't be blank")
+
+    record.title = 'Something'
+    record.valid?
+    record.errors[:title]= :should_not, include("can't be blank")
+  end
+
+end
+
+
