61174031cc019e54ee5c41f9e1f8482654d8b004
[tseferi] <[teala.seferi@cns.uist.edu.mk]>
2019-11-04 12:19:05 +0100

    Residence model updated

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -5,18 +5,18 @@ class Residence < ApplicationRecord
   end
 
   validates :title, length: {is: 5}
-  validates :description, length: {minimum: 25}
+  # validates :description, length: {minimum: 25}
   validates :available, length: {minimum: 1}
   validates :unit, length: {is: 4}
   validates :rent, length: {in: 4..6}
+
+  validates :description, length: {minimum: 25, 
+    too_short: "%{count} description must be at least 25 words"}
 
   validates_each :title, :description do |category, attr, value|
     category.errors.add(attr, 'must start with upper case')if
         value =~ /\ A [[:lower:]]/
   end
-  validates_each :description do|category, attr, value|
-    if words < 24
-      category.errors.add(attr, 'description must be at least 25 words')
-    end
-  end
+
+
 end
